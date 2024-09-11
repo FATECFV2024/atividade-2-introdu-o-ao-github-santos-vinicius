2c4287faaa9b0de035df80917e6886385d916569
Vinicius <santosviniv@gmail.com>
2024-09-10 23:20:11 -0300

    [feat] resolução atividade #1 - dados aluno
    Nome completo / RGM / Idade / Links / Turma

---
+++ FazerVsCode/src/ra2920482411048.java
@@ -0,0 +1,18 @@
+public class ra2920482411048 {
+  public static void main(String[] args){
+    String fullName = "Vinicius Silva Santos";
+    String RGM = "2920482411048";
+    int age = 25;
+    String githubUser = "@santos-vinicius";
+    String linkedinUser = "@santos-vinicius";
+    String course = "Análise e Desenvolvimento de Sistemas";
+    int semester = 2;
+
+    System.out.println("Nome Completo: " + fullName);
+    System.out.println("RA: " + RGM);
+    System.out.println("Idade: " + age + " anos");
+    System.out.println("Github: github.com/" + githubUser);
+    System.out.println("Linkedin: linkedin.com/in/" + linkedinUser);
+    System.out.println("Curso e Turma: " + course + " - " + semester + " semestre.");
+  }
+}
